1506bf875f6001569b1912c1f08d8fc5902f2a07
RAZI AHMAD <60400893+Razihmad@users.noreply.github.com>
2021-02-24 21:34:34 +0530

    model for questions created

--- quizmaker/quizapp/models.py
+++ quizmaker/quizapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.Model):
+    question = models.CharField(max_length=2000)
+    opt1 = models.CharField(max_length=200)
+    opt2 = models.CharField(max_length=200)
+    opt3 = models.CharField(max_length=200)
+    opt4 = models.CharField(max_length=200)
+    correct = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.pk
+
+
+    
