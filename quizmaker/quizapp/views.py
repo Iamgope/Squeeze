ccf690990141c3c676b3225b4c064deede5dc87d
Manish Kumar Gupt <57751918+mingul26@users.noreply.github.com>
2021-02-25 13:17:59 +0530

    populate template

--- quizmaker/quizapp/views.py
+++ quizmaker/quizapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+# create a dictionary 
+context = { 
+    "quiz" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+} 
+    # return response 
+    # return render(request, "geeks.html", context) 
+
 # Create your views here.
 def landingPage(request):
     return render(request, 'landing.html', {})
 def signinPage(request):
-    return render(request, 'give_quiz.html', {})
+    return render(request, 'give_quiz.html', context)
