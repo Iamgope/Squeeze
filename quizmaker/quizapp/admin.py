5ce3f6222ac73b177cf7eeed92b8f60c501a60a3
RAZI AHMAD <60400893+Razihmad@users.noreply.github.com>
2021-02-25 12:44:59 +0530

    quiz models is created and registered

--- quizmaker/quizapp/admin.py
+++ quizmaker/quizapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Questions
+from .models import Questions,Quizzes
 # Register your models here.
 admin.site.register(Questions)
+
+admin.site.register(Quizzes)
